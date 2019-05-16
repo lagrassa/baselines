81d933d2eb608f070da0b53dc13975b3ce105672
Alex LaGrassa <lagrassa@mit.edu>
2019-05-16 18:40:37 -0400

    added ppo run file

--- ppo2_manual.sh
+++ ppo2_manual.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-for i in 1 2 3
+for i in 1 2 3 4
 do
-    python trainable.py  FetchReach-v1 AL47 0 0 0.05 ppo2 hyperparams/ppo2FetchReach-v1AL47obs_0act_0rw_0.05best_hyperparams.npy $i manual &
-    python trainable.py  FetchReach-v1 AL47 0 0 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL47obs_0act_0rw_0.05best_hyperparams.npy $i manual &
-    python trainable.py  FetchReach-v1 AL47 0 0 0.1 ppo2 hyperparams/ppo2FetchReach-v1AL47obs_0act_0rw_0.05best_hyperparams.npy $i manual &
-    python trainable.py  FetchReach-v1 AL47 0.01 0 0.05 ppo2 hyperparams/ppo2FetchReach-v1AL47obs_0act_0rw_0.05best_hyperparams.npy $i manual &
-    python trainable.py  FetchReach-v1 AL47 0.1 0 0.05 ppo2 hyperparams/ppo2FetchReach-v1AL47obs_0act_0rw_0.05best_hyperparams.npy $i manual &
-    python trainable.py  FetchReach-v1 AL47 0 0.01 0.05 ppo2 hyperparams/ppo2FetchReach-v1AL47obs_0act_0rw_0.05best_hyperparams.npy $i manual &
-    python trainable.py  FetchReach-v1 AL47 0 0.1 0.05 ppo2 hyperparams/ppo2FetchReach-v1AL47obs_0act_0rw_0.05best_hyperparams.npy $i manual &
+    python trainable.py  FetchReach-v1 AL69 0 0 0.05 ppo2 hyperparams/ppo2FetchReach-v1AL57bobs_0act_0rw_0.05best_hyperparams.npy $i 0.0 manual &
+    sleep 1s
+    python trainable.py  FetchReach-v1 AL69 0 0 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL57bobs_0act_0rw_0.05best_hyperparams.npy $i 0.0 manual &
+    sleep 1s
+    python trainable.py  FetchReach-v1 AL69 0 0 0.1 ppo2 hyperparams/ppo2FetchReach-v1AL57bobs_0act_0rw_0.05best_hyperparams.npy $i 0.0 manual &
 done
-
-
-
-
 
 
