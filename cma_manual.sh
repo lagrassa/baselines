8e28796c771a0bd5156a7d7a801335c18df8a050
Alex LaGrassa <lagrassa@mit.edu>
2019-07-17 21:56:41 -0400

    major plotting changes

--- cma_manual.sh
+++ cma_manual.sh
@@ -3,29 +3,27 @@ for i in 1 2 3 4 5 6 7
 do
     #python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.05 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
     #sleep 1s
-    #python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    #sleep 1s
+    python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    sleep 1s
     #python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.1 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
     #sleep 1s
     #actuation noise
-    python trainable.py  FetchReach-v1 AL74 0.0 0.01 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.0 0.1 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.0 0.2 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.0 0.5 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.0 0.1 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.0 1.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.0 2.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #sleep 1s
     #obs noise
 
-    python trainable.py  FetchReach-v1 AL74 0.001 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.1 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.2 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.5 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.01 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.1 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.2 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.5 0.0 0.08 cma hyperparams/cmaFetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #sleep 1s
 
 
 
--- ppo2_manual.sh
+++ ppo2_manual.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
-for i in 1 2 3 4 5 6 7 
+for i in 6 8 9 10 
 do
-    #python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.05 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.05 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
     #sleep 1s
-    #python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
     #sleep 1s
-    #python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.1 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #python trainable.py  FetchReach-v1 AL74 0.0 0.0 0.1 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
     #sleep 1s
     #actuation noise
-    python trainable.py  FetchReach-v1 AL74 0.0 0.01 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    #python trainable.py  FetchReach-v1 AL74 0.0 0.01 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.0 0.1 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.0 0.2 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.0 0.5 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
+    #sleep 1s
+    python trainable.py  FetchReach-v1 AL74 0.0 2 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
     sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.0 0.1 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.0 0.2 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.0 0.5 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    python trainable.py  FetchReach-v1 AL74 0.0 1 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
     sleep 1s
     #obs noise
 
-    python trainable.py  FetchReach-v1 AL74 0.001 0.0 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
+    python trainable.py  FetchReach-v1 AL74 0.01 0.0 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
     sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.1 0.0 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.2 0.0 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
-    python trainable.py  FetchReach-v1 AL74 0.5 0.0 0.08 ppo2 hyperparams/ppo2FetchReach-v1AL74bobs_0act_0rw_0.05rew_noise_std_0.0best_params_so_far.npy $i 0.0 manual &
-    sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.1 0.0 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.2 0.0 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
+    #sleep 1s
+    #python trainable.py  FetchReach-v1 AL74 0.5 0.0 0.08 ppo2 hyperparams/test_ppo2frparams.npy $i 0.0 manual &
+    #sleep 1s
 
 
 
